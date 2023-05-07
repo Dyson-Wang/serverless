fe604710bdae565bb80eb1cfe0b0e03a29a205aa
wangxinghao <dyson.wang@outlook.com>
2023-05-07 20:10:55 +0800

    update5.7fin1

--- bnd/mysql.sql
+++ bnd/mysql.sql
@@ -10,3 +10,17 @@ flush privileges;
 CREATE DATABASE express;
 USE express;
 CREATE TABLE faasInfo(faasName char(16) primary key, owner char(32), namespace varchar(255), createTime timestamp, invokeTimes int);
+
+create table faasinfo(
+faasname char(16) primary key,
+namespace varchar(255) not null,
+createtime timestamp,
+invoketimes int default 0
+);
+
+create table namespace(
+owner char(32) not null,
+namespace varchar(255) not null,
+createtime timestamp not null,
+primary key(owner, namespace)
+);
