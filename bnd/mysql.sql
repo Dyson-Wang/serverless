bffff4d8993db322d7ba626f95dc349dcc48ba7a
wangxinghao <dyson.wang@outlook.com>
2023-05-15 21:24:47 +0800

    update5.15fin 1x

--- bnd/mysql.sql
+++ bnd/mysql.sql
@@ -15,17 +15,31 @@ flush privileges;
 USE express;
 SET SQL_SAFE_UPDATES = 0;
 create table faasinfo(
-faasname char(16) primary key,
-namespace varchar(255) not null,
-owner char(32) not null,
-createtime timestamp,
-invoketimes int default 0
+    faasname char(16) primary key,
+    namespace varchar(255) not null,
+    owner char(32) not null,
+    createtime timestamp,
+    invoketimes int default 0
 );
 create table namespace(
-owner char(32) not null,
-namespace varchar(255) not null,
-createtime timestamp not null,
-dbusername varchar(255),
-dbpassword varchar(255),
-primary key(owner, namespace)
+    owner char(32) not null,
+    namespace varchar(255) not null,
+    createtime timestamp not null,
+    dbusername varchar(255),
+    dbpassword varchar(255),
+    dbhost varchar(255),
+    dbname varchar(255),
+    dbport varchar(255),
+    primary key(owner, namespace)
 );
+create table totalinfo(
+    no int primary key,
+	nscount int,
+    fscount int,
+    httpget int,
+    httppost int,
+    sqlservice int,
+    totalsc int,
+    totalfail int
+);
+insert into totalinfo(no,nscount, fscount,httpget,httppost,sqlservice,totalsc,totalfail) values(1, 0, 0, 0, 0, 0, 0, 0);
