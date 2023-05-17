3bebcae3488448432acd1df96496a9989f177421
wangxinghao <dyson.wang@outlook.com>
2023-05-17 09:16:10 +0800

    fix 1.1x

--- bnd/app.js
+++ bnd/app.js
@@ -66,7 +66,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  console.log(err)
+  console.log(err.message)
   res.send('error');
 });
 
